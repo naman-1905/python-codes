7b82e5852aada74487f616284c4b77b3edcde418
Naman Chaturvedi <namanoj19@gmail.com>
2024-04-01 17:18:47 +0530

    Data Types

---
+++ Hospital.py
@@ -0,0 +1,22 @@
+# We check in a new patient in a hospital named John Smith. His age is 20 and is a new patient.
+# Write a program to define his name, age and check if he is new or not.
+
+patient_name = 'John Smith' # Strings are defined with quotes. Variables are used to store value in memory.
+patient_age = 20 # Integer value stored in variable
+is_new_patient = True #Boolean Value stored in variable.
+
+print (patient_name, patient_age, is_new_patient)
+
+#The variables are lower case and use underscore to seperate different words in the name.
+#Python is case sensitive.
+
+#The above variables, integers, floats and booleans are termed as data types in a programming language.
+
+
+
+
+
+
+
+
+
