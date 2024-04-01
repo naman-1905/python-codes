3d758fdf18110d4e0a5aa1342ce7f4ef5ddf57c1
Naman Chaturvedi <namanoj19@gmail.com>
2024-04-01 16:23:53 +0530

    Hello World

---
+++ Hello World.py
@@ -0,0 +1,10 @@
+#Python Program to print a string
+#Author: Naman Chaturvedi
+
+print ("Hello World")
+print ('My name is Naman.')
+print("This is Python")
+
+
+# Python uses .py extension.
+# Python does not use (;) like Dart and is an interpreter language.
