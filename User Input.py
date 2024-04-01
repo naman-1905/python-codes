c702b4948bf8d8a635bddd64b46669755cda167d
Naman Chaturvedi <namanoj19@gmail.com>
2024-04-01 17:30:03 +0530

    User Input

---
+++ User Input.py
@@ -0,0 +1,9 @@
+#Taking User Input in Python
+#Take user input in python to print a message like "Naman likes Cyan color."
+
+name = input ("What is your name? ") #Taking user input for his/her name
+color = input ("What is your favourite color? ") #Taking user input for his/her fav color choice
+
+print (name + " likes " + color)
+#Printing the message for him/her.
+
