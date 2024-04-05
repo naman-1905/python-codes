e673c6aa647d4a48e353e19a7c0c71afc9ea7037
Naman Chaturvedi <namanoj19@gmail.com>
2024-04-05 13:14:55 +0530

    Type Conversion

---
+++ Age Calculator.py
@@ -0,0 +1,6 @@
+#The Code to calculate age of the person based on user input and specific data type as input. 
+
+birth_year = int (input('Birth Year: '))
+current = int (input("What's the current year? "))
+age = current - birth_year
+print (age)
